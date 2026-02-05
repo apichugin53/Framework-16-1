b4aea239ff905aba6a5a74b74185f365951dbe7e
apichugin53 <167225375+apichugin53@users.noreply.github.com>
2026-02-05 23:49:15 +0500

    Add files via upload

--- users/admin.py
+++ users/admin.py
@@ -1,7 +1,11 @@
 from django.contrib import admin
-from models import CustomUser
-
+from django.contrib.auth.admin import UserAdmin
+from .models import CustomUser
 
 @admin.register(CustomUser)
-class CustomUserAdmin(admin.ModelAdmin):
-    list_display = ('username', 'email', 'age')
+class CustomUserAdmin(UserAdmin):
+    list_display = ('username', 'email', 'first_name', 'last_name')
+    fieldsets = (
+        *UserAdmin.fieldsets,
+        ('Дополнительные поля', {'fields': ('phone_number', 'birth_date')}),
+    )
--- users/models.py
+++ users/models.py
@@ -1,10 +1,10 @@
-from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.db import models
 
 
 class CustomUser(AbstractUser):
-    age = models.PositiveIntegerField(null=True, blank=True)
-    bio = models.TextField(max_length=500, blank=True)
+    phone_number = models.CharField(max_length=15, blank=True, null=True)
+    birth_date = models.DateField(blank=True, null=True)
 
     def __str__(self):
         return self.username
